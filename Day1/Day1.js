036c7eac7cf2225c8c0c75dd7ed887c3991f4c84
Nush100 <nushkavi123@gmail.com>
2022-04-20 11:19:18 +0530

    Challenge Day1

--- Day1/Day1.js
+++ Day1/Day1.js
@@ -1,32 +1,26 @@
-
-//This function returns the reversed string value, when a string is passed as an argument
-const reverseString = (word) => {
+String.prototype.reverseString = function() {
 
     //create a constant to assign word length
-    const wordLength = word.length;
+    const wordLength = this.length;
 
     //check whether the string is empty
     if(wordLength < 1 ) throw Error ("The string is empty");
 
     //define leters array
     let letters = [];
-            
+
     /*assign values for letters array */
     for(let i = 0; i < wordLength; i++){
 
         // the value of lettershould be the char value at wordLength - (i+1)
-        letters[i] = word.charAt(wordLength - i - 1);
+        letters[i] = this.charAt(wordLength - i - 1);
 
     }
 
     //join("") use to join the values of the array without commas
-    return letters.join("");     
-
+    return letters.join("");
+    
 }
 
-//to call the function
-//reverseString("abcde");
-
-
-
- 
+//To call the function
+//"abcde".reverseString();
