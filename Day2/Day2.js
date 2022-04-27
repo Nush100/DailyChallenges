de3b89b3299de2c03ecf9dc135219ea7c2171986
Nush100 <nushkavi123@gmail.com>
2022-04-27 09:58:02 +0530

    Chalenge Day2

--- Day2/Day2.js
+++ Day2/Day2.js
@@ -1,25 +1,44 @@
+// const Intersection = (first, second) => {
+
+//     //initialize the pointers
+//     let commanElements = [];
+
+//     //throw an error if an array is empty 
+//     if(first.length == 0 || second.length == 0) throw Error ("An array is empty");
+ 
+//     //push values to the commonElements array 
+//     for(let i = 0; i < first.length; i++) {
+
+//         for(let j = 0; j < second.length; j++) {
+//             if(first[i] == second[j]) commanElements.push(first[i]);
+//         }
+
+//     }
+         
+//     //return commonElements
+//     return commanElements; 
+// }
+
+// //call the function
+//Intersection([1, 2, 5, 9, 8], [2]);
+
 const Intersection = (first, second) => {
 
-    //initialize the pointers
     let commanElements = [];
-    let min;
+    let max;
 
     //get the minimum value
-    if(first.length <= second.length) min = first.length;
-    else min = second.length;
- 
-    //throw an error if an array is empty 
-    if(min == 0) throw Error ("One array is empty");
- 
-    //push values to the commonElements array 
-    for(let i = 0; i < min; i++)
-        if(second.includes(first[i])) commanElements.push(first[i])  
-  
+    if(first.length >= second.length) max = first.length;
+    else max = second.length;
+
+    //push values to the commonElements array
+    for(let i = 0; i < max; i++)
+        if(second.includes(first[i])) commanElements.push(first[i]);
+
     //return commonElements
-    return commanElements; 
+    return commanElements;
+
 }
 
- 
-
 //call the function
-//Intersection([1, 3, 5, 9, 8], [6, 2, 5, 9, 0, 8, 8]);
+//Intersection([5], [1, 3, 5, 9, 8]));
